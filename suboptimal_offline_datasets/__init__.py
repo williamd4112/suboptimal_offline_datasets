bcc9b8676641b60ee6cfa7a62f7d1f54fab09a9c
Zhang-Wei Hong <zwhong@katsudon.csail.mit.edu>
2023-05-09 18:59:05 -0400

    add reset free dataset

--- suboptimal_offline_datasets/__init__.py
+++ suboptimal_offline_datasets/__init__.py
@@ -66,12 +66,23 @@ for agent in ['hopper', 'halfcheetah', 'ant', 'walker2d']:
                 )
 
     # mixed-varying-n
-    for dataset in ['medium', 'expert']:
+    for dataset in [
+            "medium",
+            "expert",
+            "medium-expert",
+            "full-replay",
+            "medium-replay",
+        ]:
         for version in ['v2']:
             score_env_name = '%s-%s-%s' % (agent, dataset, version)
-            for ratio in ["0.01", "0.05", "0.1", "0.5"]:
+            for ratio in ["0.01", "0.05", "0.1", "0.5", "1.0"]:
+                if ratio != "1.0" and dataset in ["medium-expert", "full-replay", "medium-replay"]:
+                    continue
                 for n in [int(1e5), int(5e4), int(2e4), int(1e4), int(5e3)]:
-                    env_name = '%s-random-%s-%s-%d-%s' % (agent, dataset, ratio, n, version)
+                    if ratio == "1.0":
+                        env_name = '%s-%s-%s-%d-%s' % (agent, dataset, ratio, n, version)
+                    else:
+                        env_name = '%s-random-%s-%s-%d-%s' % (agent, dataset, ratio, n, version)
                     register(
                         id=env_name,
                         entry_point='suboptimal_offline_datasets.custom_envs:get_%s_env' % agent.replace('halfcheetah', 'cheetah').replace('walker2d', 'walker'),
@@ -105,9 +116,9 @@ for agent in ['hopper', 'halfcheetah', 'ant', 'walker2d']:
                 )
 
     # reset-stoch
-    for dataset in ['medium', 'expert']:
+    for dataset in ['medium', 'expert', "random"]:
         for version in ['v2']:
-            for mode in ["reset-stoch", "resetfree-stoch", "resetfree-det"]:
+            for mode in ["reset-stoch", "resetfree-stoch", "resetfree-det", "resetfree"]:
                 score_env_name = '%s-%s-%s' % (agent, dataset, version)
                 register(
                     id=f'{agent}-{dataset}-{mode}-{version}',
