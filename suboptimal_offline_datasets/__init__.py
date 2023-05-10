a8071e201fa5480fa561834f472a5f188d09e5cf
Zhang-Wei Hong <zwhong@katsudon.csail.mit.edu>
2023-05-10 07:58:03 -0400

    fix

--- suboptimal_offline_datasets/__init__.py
+++ suboptimal_offline_datasets/__init__.py
@@ -75,7 +75,7 @@ for agent in ['hopper', 'halfcheetah', 'ant', 'walker2d']:
                         'deprecated': version != 'v2',
                         'ref_min_score': infos.REF_MIN_SCORE[score_env_name],
                         'ref_max_score': infos.REF_MAX_SCORE[score_env_name],
-                        'dataset_url': os.path.join(init_path, "partial_mixed_datasets", '%s-random-%s-partial-%s-5-10-%s' % (agent, dataset, ratio, version) + ".hdf5")
+                        'dataset_url': os.path.join(init_path, "partial_mixed_datasets", '%s-random-%s-%s-5-10-%s' % (agent, dataset, ratio, version) + ".hdf5")
                     }
                 )
 
